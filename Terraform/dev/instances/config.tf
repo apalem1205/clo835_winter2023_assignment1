c88d6b51cc8fac1a3698eb125d7f36719c1a6b00
apalem1205 <99748602+apalem1205@users.noreply.github.com>
2023-01-28 21:53:35 -0500

    Delete Terraform/dev/instances directory

--- Terraform/dev/instances/config.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "clo835-assignment1"                
-    key    = "networking/terraform.tfstate" 
-    region = "us-east-1"                          
-  }
-}
