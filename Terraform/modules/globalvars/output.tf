ef5ea135465326579d11c789c6ee5f3e79fa67a5
apalem1205 <99748602+apalem1205@users.noreply.github.com>
2023-01-28 22:01:35 -0500

    Create output.tf

---
+++ Terraform/modules/globalvars/output.tf
@@ -0,0 +1,13 @@
+# Default tags
+output "default_tags" {
+  value = {
+    "Owner" = "aparna"
+    "App"   = "Web"
+    "Project" = "CLO835"
+  }
+}
+
+# Prefix to identify resources
+output "prefix" {
+  value     = "assignment1"
+}
