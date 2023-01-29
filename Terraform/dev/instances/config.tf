2c208494a9de6bb390f7c18e199d1928e1bfadc3
apalem1205 <99748602+apalem1205@users.noreply.github.com>
2023-01-28 21:50:23 -0500

    Create config.tf

---
+++ Terraform/dev/instances/config.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "s3" {
+    bucket = "clo835-assignment1"                
+    key    = "networking/terraform.tfstate" 
+    region = "us-east-1"                          
+  }
+}
