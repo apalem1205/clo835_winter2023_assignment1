bae3159adf6546d3f28598711b137422efb8384c
apalem1205 <99748602+apalem1205@users.noreply.github.com>
2023-02-02 22:35:38 -0500

    Update config.tf

--- Terraform/dev/instances/config.tf
+++ Terraform/dev/instances/config.tf
@@ -1,7 +1,1 @@
-terraform {
-  backend "s3" {
-    bucket = "clo835-assignment1"                
-    key    = "networking/terraform.tfstate"
-    region = "us-east-1"                           
-  }
-}
+
