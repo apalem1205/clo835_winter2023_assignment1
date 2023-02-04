301b4f1fccba875d88861a8b4dbbec948179a726
apalem1205 <99748602+apalem1205@users.noreply.github.com>
2023-02-04 17:22:34 -0500

    Update main.tf

--- Terraform/dev/instances/main.tf
+++ Terraform/dev/instances/main.tf
@@ -79,8 +79,8 @@ resource "aws_security_group" "my_sg" {
   
   ingress {
     description      = "SSH from everywhere"
-    from_port        = 8080
-    to_port          = 8080
+    from_port        = 8081
+    to_port          = 8081
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
@@ -88,8 +88,16 @@ resource "aws_security_group" "my_sg" {
   
   ingress {
     description      = "SSH from everywhere"
-    from_port        = 8081
-    to_port          = 8081
+    from_port        = 8082
+    to_port          = 8082
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    description      = "SSH from everywhere"
+    from_port        = 8083
+    to_port          = 8083
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
